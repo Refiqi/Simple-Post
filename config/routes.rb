980dafcfae7d94accf9707dfbebbc800a06d3633
RefiqiMF <refyfadhila96@gmail.com>
2018-10-31 13:50:31 +0700

    Adding Existing File

---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+	root 'posts#index', as: 'home'
+    get 'index/id' => 'posts#new' , as: :new
+  
+	
+	resources :posts  do
+		resources :comments
+	end
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
